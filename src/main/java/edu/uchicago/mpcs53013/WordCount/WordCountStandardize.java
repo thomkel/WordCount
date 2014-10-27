426a7ff6d80068253f01bcc00b080089ba597250
Thomas Kelly <thomkel@uchicago.edu>
2014-10-27 12:04:30 -0500

    added WordCountStandardize to remove punctuation, set strings to lowercase

---
+++ src/main/java/edu/uchicago/mpcs53013/WordCount/WordCountStandardize.java
@@ -0,0 +1,130 @@
+package edu.uchicago.mpcs53013.WordCount;
+
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+public class WordCountStandardize {
+
+	public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+		private final static IntWritable one = new IntWritable(1);
+		private Text word = new Text();
+
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+			String line = value.toString();
+			StringTokenizer tokenizer = new StringTokenizer(line);
+			while (tokenizer.hasMoreTokens()) {
+//				word.set(tokenizer.nextToken());
+				
+				// code found http://stackoverflow.com/questions/18830813/how-can-i-remove-punctuation-from-input-text-in-java
+				String inputString = tokenizer.nextToken().replaceAll("[^a-zA-Z ]", "").toLowerCase();
+				word.set(inputString);
+				context.write(word, one);				
+				
+				// modifying code
+//				char[] chars = word.toString().toCharArray();
+//				for(char ch: chars){
+//					String charType = getCharType(ch);
+//					Text textType = new Text();
+//					textType.set(charType);
+//					context.write(textType, one);
+//					
+//				}
+						
+//				context.write(word, one);
+			}
+		}
+		
+//		public String getCharType(char ch){
+//			if(Character.isDigit(ch)){
+//				return "digit";
+//			}
+//			else if(Character.isLetter(ch)){
+//				if(Character.isLowerCase(ch)){
+//					return "lowercase";
+//				}
+//				else{
+//					assert(Character.isTitleCase(ch));
+//					return "uppercase";
+//				}
+//			}
+//			else if(Character.isSpaceChar(ch)){
+//				return "space";
+//			}
+//			else{
+//				assert(Character.isDefined(ch));
+//				return "punctuation";
+//			}
+//			
+//		}
+	} 
+
+	public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+		public void reduce(Text key, Iterable<IntWritable> values, Context context) 
+				throws IOException, InterruptedException {
+			int sum = 0;
+			for (IntWritable val : values) {
+				sum += val.get();
+			}
+			context.write(key, new IntWritable(sum));
+		}
+	}
+	static String hadoopPrefix = System.getenv("HADOOP_PREFIX");
+	public static void main(String[] args) {
+		if(hadoopPrefix == null) {
+			throw new RuntimeException("Please set HADOOP_PREFIX environment variable");
+		}
+		Configuration conf = new Configuration();
+		conf.addResource(new Path(hadoopPrefix + "/conf/core-site.xml"));
+
+		Job job;
+		try {
+			job = new Job(conf, "wordcount");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return;
+		}
+
+		job.setJarByClass(WordCountStandardize.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+
+		job.setMapperClass(Map.class);
+		job.setReducerClass(Reduce.class);
+
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+
+		try {
+			FileInputFormat.addInputPath(job, new Path(args[0]));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		try {
+			job.waitForCompletion(true);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
